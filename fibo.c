d0147d6c9b607ddd55044b968ca07ee0394f283e
Julius Chesoni <juliuschesoni79@gmail.com>
2026-07-05 19:57:27 -0500

    Given 1, 2, 3; to generate the next, the next thereafter, and the next ...
    Added comments for date, calculation method, and a note of appreciation.

---
+++ fibo.c
@@ -0,0 +1,32 @@
+//DATE: 7/5/2026
+//NEXT = CURRENT * 2 - [NUMBER 2 POSITIONS BEFORE]
+// 1, 2, 3, 5, 8, 13, 21, 34, ...
+//Glory be to the Creator, the One who sustains us, the one who lives forever.
+
+
+#include <stdio.h>
+
+int main() {
+    int n = 20; // Number of Fibonacci numbers to generate
+
+    long long fib[n];
+
+    // Initial values
+    fib[0] = 1;
+    fib[1] = 2;
+    fib[2] = 3;
+
+    printf("Fibonacci Sequence:\n");
+    printf("%lld %lld %lld ", fib[0], fib[1], fib[2]);
+
+    // Generate using:
+    // next = 2 * current - previous_previous
+    for (int i = 3; i < n; i++) {
+        fib[i] = 2 * fib[i - 1] - fib[i - 3];
+        printf("%lld ", fib[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
